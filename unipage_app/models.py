ddae09e3e6f2299d290de3b0bc39db84dfef298e
Cheryl Guo <guocheng0@gmail.com>
2017-02-28 16:14:22 -0500

    first steps

--- unipage_app/models.py
+++ unipage_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Fact(models.Model):
+    university=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.university
+
+
